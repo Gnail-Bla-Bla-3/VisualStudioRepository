99cefaf8562b21c9673c6d536afe3dc451895207
Gnail-Bla-Bla-3 <123532344+Gnail-Bla-Bla-3@users.noreply.github.com>
2023-05-27 00:01:59 -0700

    May 27

--- Project1/Project1/main.cu
+++ Project1/Project1/main.cu
@@ -3,22 +3,42 @@
 #include <thrust\host_vector.h>
 #include <cstdlib>
 #include <thrust\reduce.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#undef main
 
-#define mormillion 100
+#define mormillion 1000000
 
 int main() {
+	int startOfLoop = SDL_GetTicks();
+
+
 	thrust::device_vector<int> dv(0);
 	thrust::host_vector<int> hv(0);
-	float finalValue = 0;
-	for (int i = 0; i < mormillion; i++) {
-		dv.push_back(rand());
-	}
+	int finalValue = 0;
+	//for (int i = 0; i < mormillion; i++) {
+	//	dv.push_back(rand());
+	//}
+	
+	int firstLoop = SDL_GetTicks() - startOfLoop;
 
 	std::cout << "Made it past the Random Part!" << std::endl;
 	
-	finalValue = thrust::reduce(dv.begin(), dv.end());
+	//finalValue = thrust::reduce(dv.begin(), dv.end());
+
+	int secondStart = SDL_GetTicks();
+
+	for (int i = 0; i < mormillion; i++) {
+		hv.push_back(rand());
+	}
+
+	int SecondValue = thrust::reduce(hv.begin(), hv.end());
 	
-	std::cout << finalValue << std::endl;
+	int SecondFinal = SDL_GetTicks() - secondStart;
+
+	std::cout << finalValue << ", " << SecondValue <<std::endl;
+	std::cout << "GPU Vector Time: " << finalValue << std::endl;
+	std::cout << "CPU Vector Time: " << SecondFinal << std::endl;
 
 	return 0;
 }
